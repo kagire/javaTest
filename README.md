7ee82353f545f111ce5c878dc2e98b35f7d132e3
Roma <49759326+kagire@users.noreply.github.com>
2020-11-14 12:29:39 +0300

    minor UI edit

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # If you have any questions...
 ### About database
-Column `employee_id` in my **employeedb** is a Primary Key, that is important info to understand why im inserting fields like i do.
+Column `employee_id` in my **employeedb** is a Primary Key, that is important info to understand why im inserting fields like I do.\
+SQL script to insert fields if you want your db not to be empty:
+```postgres-sql
+insert into employee (date_of_birth, department_id, first_name, gender, job_title, last_name)
+values (TO_DATE('1/1/2011', 'DD/MM/YYYY'), 80, 'Sarah', 'FEMALE', 'Security manager', 'Andersen');
 
+insert into employee (date_of_birth, department_id, first_name, gender, job_title, last_name)
+values (TO_DATE('5/10/2002', 'DD/MM/YYYY'), 2, 'Miles', 'MALE', 'Spider-man', 'Morales');
+```
 ### About tests
-Test provided to sevice and connection. [Fast link to tests](https://github.com/kagire/javaTest/tree/main/src/test/java/com/mastery/simplewebapp "Fast link to tests")
+Test provided to service and connection. [Fast link to tests](https://github.com/kagire/javaTest/tree/main/src/test/java/com/mastery/simplewebapp "Fast link to tests")
 
 ### About UI
-Its not fully-functional, but still can show ajax-requests are working, POST and GET methods are showed.  [Fast link to UI](https://github.com/kagire/javaTest/tree/main/src/main/resources/static "Fast link to UI")
+It's not fully-functional, but still can show ajax-requests are working, showing POST and GET methods.  [Fast link to UI](https://github.com/kagire/javaTest/tree/main/src/main/resources/static "Fast link to UI")
 
-> You need to delete annotation `@RequestBody` from EmployeeController->newEmployee method to accept ajax request from html form
+> You need to delete `@RequestBody` annotation from EmployeeController->newEmployee method to accept ajax JSON from html form
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,34 +1,56 @@
 $(document).ready(function() {
+
+    var b = document.createElement('button');
+    b.innerHTML = "Add employee";
+
     $.ajax({
           type: "GET",
           url: "http://localhost:8080/employees"
     }).then(function(data) {
-        var counter = 1;
-        for (const value of Object.values(data)) {
-            var counterString = counter.toString();
-
-            var employeeIdString = '.employee-id-' + counterString;
-            var nameString = '.first-name-' + counterString;
-            var lastNameString = '.last-name-' + counterString;
-            var departmentIdString = '.department-id-' + counterString;
-            var jobTitleString = '.job-title-' + counterString;
-            var genderString = '.gender-' + counterString;
-            var dateString = '.date-' + counterString;
-
-            $(employeeIdString).append(value.employeeId);
-            $(nameString).append(value.firstName);
-            $(lastNameString).append(value.lastName);
-            $(departmentIdString).append(value.departmentId);
-            $(jobTitleString).append(value.jobTitle);
-            $(genderString).append(value.gender);
-            $(dateString).append(value.dateOfBirth);
-
-            console.log(value.employeeId);
+        var counter = 0;
+        var body = document.getElementsByTagName('body')[0];
+        var tbl = document.createElement('table');
+        tbl.style.width = '100%';
+        tbl.setAttribute('border', '1');
+        var tbdy = document.createElement('tbody');
+      for (const value of Object.values(data)) {
             counter++;
-        }
-        $('.count').append(counter - 1);
+            var tr = document.createElement('tr');
+            var td1 = document.createElement('td');
+            td1.appendChild(document.createTextNode(value.employeeId));
+            var td2 = document.createElement('td');
+            td2.appendChild(document.createTextNode(value.firstName));
+            var td3 = document.createElement('td');
+            td3.appendChild(document.createTextNode(value.lastName));
+            var td4 = document.createElement('td');
+            td4.appendChild(document.createTextNode(value.departmentId));
+            var td5 = document.createElement('td');
+            td5.appendChild(document.createTextNode(value.jobTitle));
+            var td6 = document.createElement('td');
+            td6.appendChild(document.createTextNode(value.gender));
+            var td7 = document.createElement('td');
+            td7.appendChild(document.createTextNode(value.dateOfBirth));
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
+            tr.appendChild(td6);
+            tr.appendChild(td7);
+            tbdy.appendChild(tr);
+      }
+      var tr1 = document.createElement('tr');
+      var th1 = document.createElement('th');
+      th1.appendChild(document.createTextNode("Total: " + counter));
+      tr1.appendChild(th1);
+      tbdy.appendChild(tr1);
+      
+      tbl.appendChild(tbdy);
+      body.appendChild(tbl);
+
+    body.appendChild(b); 
     });
-    $(".newEmployee").click(function(){
+    b.onclick = function(){
         var sendInfo = {
             "employeeId": 4,
             "firstName": "New",
@@ -42,35 +64,52 @@ $(document).ready(function() {
         $.ajax({
             type: "POST",
             url: "http://localhost:8080/employees",
-            contentType: 'application/x-www-form-urlencoded',
+            dataType: "json",
             data: sendInfo,
             context: document.body
         }).then(function(data) {
-          var counter = 1;
+            var counter = 0;
+            var body = document.getElementsByTagName('body')[0];
+            var tbl = document.createElement('table');
+            tbl.style.width = '100%';
+            tbl.setAttribute('border', '1');
+            var tbdy = document.createElement('tbody');
           for (const value of Object.values(data)) {
-              var counterString = counter.toString();
-
-              var employeeIdString = '.employee-id-' + counterString;
-              var nameString = '.first-name-' + counterString;
-              var lastNameString = '.last-name-' + counterString;
-              var departmentIdString = '.department-id-' + counterString;
-              var jobTitleString = '.job-title-' + counterString;
-              var genderString = '.gender-' + counterString;
-              var dateString = '.date-' + counterString;
-
-              $(employeeIdString).append(value.employeeId);
-              $(nameString).append(value.firstName);
-              $(lastNameString).append(value.lastName);
-              $(departmentIdString).append(value.departmentId);
-              $(jobTitleString).append(value.jobTitle);
-              $(genderString).append(value.gender);
-              $(dateString).append(value.dateOfBirth);
-
-              console.log(value.employeeId);
-              counter++;
+                counter++;
+                var tr = document.createElement('tr');
+                var td1 = document.createElement('td');
+                td1.appendChild(document.createTextNode(value.employeeId));
+                var td2 = document.createElement('td');
+                td2.appendChild(document.createTextNode(value.firstName));
+                var td3 = document.createElement('td');
+                td3.appendChild(document.createTextNode(value.lastName));
+                var td4 = document.createElement('td');
+                td4.appendChild(document.createTextNode(value.departmentId));
+                var td5 = document.createElement('td');
+                td5.appendChild(document.createTextNode(value.jobTitle));
+                var td6 = document.createElement('td');
+                td6.appendChild(document.createTextNode(value.gender));
+                var td7 = document.createElement('td');
+                td7.appendChild(document.createTextNode(value.dateOfBirth));
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tr.appendChild(td3);
+                tr.appendChild(td4);
+                tr.appendChild(td5);
+                tr.appendChild(td6);
+                tr.appendChild(td7);
+                tbdy.appendChild(tr);
           }
-          $('.count').append(counter - 1);
+          var tr1 = document.createElement('tr');
+          var th1 = document.createElement('th');
+          th1.appendChild(document.createTextNode("Total: " + counter));
+          tr1.appendChild(th1);
+          tbdy.appendChild(tr1);
+          
+          tbl.appendChild(tbdy);
+          body.appendChild(tbl);
+
       });
         alert("Created");
-      });
+      };
 });
