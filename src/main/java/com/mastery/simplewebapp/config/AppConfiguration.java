806642f543cf28eafff012f5b954ebe737270a6d
Roma <49759326+kagire@users.noreply.github.com>
2020-12-02 21:02:33 +0300

    config

--- src/main/java/com/mastery/simplewebapp/config/AppConfiguration.java
+++ src/main/java/com/mastery/simplewebapp/config/AppConfiguration.java
@@ -1,4 +1,29 @@
 package com.mastery.simplewebapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class AppConfiguration {
+
+    @Value( "${spring.datasource.url}" )
+    private String jdbcUrl;
+
+    @Value( "${spring.datasource.driver-class-name}" )
+    private String jdbcClassName;
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName(jdbcClassName);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username("postgres");
+        dataSourceBuilder.password("1234");
+        return dataSourceBuilder.build();
+    }
+
 }
